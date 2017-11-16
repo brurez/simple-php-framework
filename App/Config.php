0194521cd35321a61b39157b2571d5328348f648
Bruno de Rezende <brunoderezende@hotmail.com>
2017-11-16 08:06:34 -0200

    added config file

---
+++ App/Config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bruno
+ * Date: 16/11/2017
+ * Time: 07:59
+ */
+
+namespace App;
+
+
+class Config {
+
+	const DB_HOST = 'localhost';
+
+	const DB_NAME = 'mvc';
+
+	const DB_USER = 'root';
+
+	const DB_PASSWORD = '';
+}
